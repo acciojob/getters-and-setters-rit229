567b7c2266afccd96337aa0b03b661d5231e70a3
rit229 <malikritu845@gmail.com>
2025-10-16 20:30:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,54 +1,38 @@
 //complete this code
-class Person {}
-constructor(name, age) {
-this._name = name;
-this._age = age >= 0 ? age : console.error("Age cannot be negative");
- }
-
-get name() {
-return this._name;
+class Person {
+	constructor(_name,_age){
+		this._name=_name;
+		this._age=_age;
+	}
+	get name(){
+		return this._name;
+	}
+	set age(newAge){
+		this._age=newAge;
+	}
 }
 
-get age() {
-	get age() {
-return this._age;
- }
-
-set age(age) {
-if (age < 0) {
-console.error("Age cannot be negative");
-} else {
-this._age = age;
- }
-}
-}
-class Student extends Person {}
-study() {
-console.log(`${this.name} is studying`);
+class Student extends Person {
+	study(){
+    console.log(${this._name} is studying)
+	}
 }
 
-showInfo() {
-return `${this.name}, Age: ${this.age}`;
- }
- }
-
-class Teacher extends Person {}
-teach() {
-console.log(`${this.name} is teaching`);
- }
+class Teacher extends Person {
+teach(){
+		console.log(${this._name} is teaching)
+	}
 }
-function addTeacher() {
-const name = prompt("Enter teacher name:");
-const age = prompt("Enter teacher age:");
-const teacher = new Teacher(name, parseInt(age));
+
+const person=new Person("John",25);
+console.log(person.name);
+person._age=30;
+console.log(person._age);
+const student=new Student("Alice",22);
+student.study();
+const teacher=new Teacher("Bob",40);
 teacher.teach();
-document.getElementById('info').innerHTML += `${teacher.name}, Age: ${teacher.age}<br>`;
- }
-document.getElementById('addStudent').addEventListener('click', addStudent);
-document.getElementById('addTeacher').addEventListener
-document.getElementById('addTeacher').addEventListener('click', addTeacher);
-
 // Do not change the code below this line
 window.Person = Person;
 window.Student = Student;
-window.Teacher = Teacher;
+window.Teacher = Teacher;
